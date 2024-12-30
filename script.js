dd05dd9765c0efb69365e846ba3f59dd2edbf4e9
imalikhan <142657670+imalikhan@users.noreply.github.com>
2024-12-30 23:26:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$('.matches-list-inn').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+  
+  $('.latest-news-slider-inn').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+  $('.x-feeds-slider-inn').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
